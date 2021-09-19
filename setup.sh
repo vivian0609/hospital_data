a779a57c4b59e919c482565ace8e7c1de8b3f7df
vivian0609 <1090421150@qq.com>
2021-09-19 18:26:20 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"yanqiliang609@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+enableXsrfProtection = false\n\
+port = 8501\n\
+" > ~/.streamlit/config.toml
